6b26e6c01db0e771ffc10c0d2d3ff6f92cfc1907
Jangchan0 <jchy16@gmail.com>
2022-12-28 20:03:08 +0900

    MODIFY: import

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../src/pages/Login/Login";
+import Login from "./pages/Login/Login";
 
 export default function Router() {
   return (
